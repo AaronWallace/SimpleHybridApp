b58ce8ac04c71f340b353dc8359f682c5ba4d41e
AaronWallace <aaron@leagasy.com>
2016-04-22 12:13:16 -0500

    Added some basic activities and buttons

--- app/src/main/java/specialforces/mobile/dynatrace/com/simplehybridapp/MainActivity.java
+++ app/src/main/java/specialforces/mobile/dynatrace/com/simplehybridapp/MainActivity.java
@@ -1,8 +1,7 @@
 package specialforces.mobile.dynatrace.com.simplehybridapp;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,6 +10,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,21 +19,16 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ButtonFragment.OnFragmentInteractionListener, SimpleWebViewFragment.OnFragmentInteractionListener{
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
+    private static final String LOGTAG = MainActivity.class.getSimpleName();
+
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
+    private static final String SECTION_ONE_NAME = "Buttons";
+    private static final String SECTION_TWO_NAME = "Local Content";
+    private static final String SECTION_THREE_NAME= "Dynatrace";
+
     private ViewPager mViewPager;
 
     @Override
@@ -54,17 +49,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -88,39 +73,9 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d(LOGTAG, "Fragment interaction callback...");
     }
 
     /**
@@ -137,7 +92,20 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            int tabId = position + 1;
+
+
+            switch (position) {
+                case 0:
+                    return ButtonFragment.newInstance(tabId);
+                case 1:
+                    return SimpleWebViewFragment.newInstance(tabId, "about:blank");
+                case 2:
+                    return SimpleWebViewFragment.newInstance(tabId, "http://www.dynatrace.com");
+            }
+
+            return null;
         }
 
         @Override
@@ -150,11 +118,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return SECTION_ONE_NAME;
                 case 1:
-                    return "SECTION 2";
+                    return SECTION_TWO_NAME;
                 case 2:
-                    return "SECTION 3";
+                    return SECTION_THREE_NAME;
             }
             return null;
         }
